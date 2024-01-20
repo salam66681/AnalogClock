3829bc70186c97e7229d8d3badeeea4bb5a4c79a
salam66681 <salam66681@gmail.com>
2024-01-20 17:41:38 +0530

    This is a new commit for what I originally planned to be an amendmend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+import Clock from './Clock'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Clock/>
+      </>
+    )
+  }
+}
